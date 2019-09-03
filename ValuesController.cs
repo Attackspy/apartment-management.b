71bace2d47edc33be4376e9ad725db2a478aae35
Attackspy <Attackspy@users.noreply.github.com>
2019-09-03 11:58:34 +0300

    Create ValuesController.cs

---
+++ ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apartment_management.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return   "Tarih: " + DateTime.Now.ToShortDateString() + " " + DateTime.Today.DayOfWeek.ToString() + Environment.NewLine + Environment.NewLine + Environment.NewLine + Environment.NewLine + "DUYURULAR:" ;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            if (id < 16)
+                return "Kullanıcı:" + Environment.UserDomainName + Environment.NewLine + "Bakılan Numara:" + id.ToString();
+            else
+                return "Yanlış Numara";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
