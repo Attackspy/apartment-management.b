63c78c55b71f97c8a06d380141bccada7b79b431
Attackspy <Attackspy@users.noreply.github.com>
2019-10-09 19:22:48 +0300

    1.x
    I changed all files with backup files because they were broken and changed something

--- apartment-management.b1/Controllers/ValuesController.cs
+++ apartment-management.b1/Controllers/ValuesController.cs
@@ -4,25 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace apartment_management.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        int daire = 16;
         // GET api/values
         [HttpGet]
         public ActionResult<string> Get()
         {
-            return   "Tarih: " + DateTime.Now.ToShortDateString() + " " + DateTime.Today.DayOfWeek.ToString() + Environment.NewLine + Environment.NewLine + Environment.NewLine + Environment.NewLine + "DUYURULAR:" ;
+            return $"Tarih:  {DateTime.Now.ToShortDateString()} {DateTime.Today.DayOfWeek.ToString()} {Environment.NewLine}daire sayısı: {daire} {Environment.NewLine} {Environment.NewLine}{Environment.NewLine}{Environment.NewLine}DUYURULAR:";
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            if (id < 16)
-                return "Kullanıcı:" + Environment.UserDomainName + Environment.NewLine + "Bakılan Numara:" + id.ToString();
+            if (id < daire)
+                return $"Kullanıcı: {Environment.UserDomainName} {Environment.NewLine}Bakılan Numara: {id}";
             else
                 return "Yanlış Numara";
         }
