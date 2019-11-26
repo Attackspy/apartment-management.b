00c07b467cf15e619b658fefcce1818d2503dca5
Attackspy <Attackspy@users.noreply.github.com>
2019-11-26 16:05:19 +0300

    3.0 restart
    update

---
+++ apartment-management.s1/models/users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace apartment_management.s1.models
+{
+    public class users
+    {
+
+        public string name { get; set; }
+
+        public string password { get; set; }
+
+        public string email { get; set; }
+
+    }
+}
